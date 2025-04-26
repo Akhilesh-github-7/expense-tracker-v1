2bbcb652335486a5e72fdbb2808c147e6a6dcbe6
Akhilesh-github-7 <akhileshmsskp@gmail.com>
2025-04-26 10:14:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Home from './pages/Auth/Dashboard/Home'
+import Income from './pages/Auth/Dashboard/Income'
+import Expense from './pages/Auth/Dashboard/Expense'
+import { Toaster } from 'react-hot-toast'
+import UserProvider from './context/UserContext'
+const App = () => {
+  return (
+    <UserProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Root/>}/>
+            <Route path='/login' exact element={<Login/>}/>
+            <Route path='/signup' exact element={<SignUp/>}/>
+            <Route path='/dashboard' exact element={<Home/>}/>
+            <Route path='/income' exact element={<Income/>}/>
+            <Route path='/expense' exact element={<Expense/>}/>
+          </Routes>
+        </Router>
+      </div>
+      <Toaster
+       toastOptions={{
+        className:"",
+        style:{
+          fontSize:'13px'
+        },
+       }}/>
+    </UserProvider>
+  )
+}
+
+export default App
+
+const Root = ()=>{
+  // check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token")
+
+  // Redirected to dashboard if authenticated,otherwise to login
+  return isAuthenticated?(
+    <Navigate to="/dashboard"/>
+  ):(
+    <Navigate to="/login"/>
+  )
+}
